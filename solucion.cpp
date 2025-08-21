3559316941e8e6f6e094c5ee2c43c32ec32e66bd
Eric <ericlunaseven@gmail.com>
2025-08-21 19:57:10 -0300

    Triangle Area

---
+++ solucion.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int triArea (int base, int height){
+    return (base * height) / 2;
+    
+}
+    
+int main (){
+    cout << triArea (4, 8) << endl;
+    cout << triArea (5, 7) << endl;
+}
+
+ 
