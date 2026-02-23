752a6229141021eb966e3961e45503c855e08bf9
Eric <ericlunaseven@gmail.com>
2026-02-23 14:48:59 -0300

    Refactor: Update cardCounter to use arrow function and ternary operator

--- casinoCards.js
+++ casinoCards.js
@@ -1,16 +1,14 @@
-let count = 0
-function cardCounter(card) {
+let count = 0;
+const cardCounter = (card) => {
+  
   switch(card) {
     case 2: case 3: case 4: case 5: case 6: 
-    count++;
-    break;
+      count++;
+      break;
     case 10: case "J": case "Q": case "K": case "A":    
-    count--;
-    break;
-  } if(count > 0) {
-    return count + " Bet";
-  } else {
-    return count + " Hold";
-  }
-}
-
+      count--;
+      break;
+  } 
+  
+  return count > 0 ? count + " Bet" : count + " Hold";
+};
